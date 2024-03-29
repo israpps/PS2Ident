9754a617b92d93dc1440902d0b5a72463ef93ab3
AKuHAK <621640+AKuHAK@users.noreply.github.com>
2024-03-29 10:58:26 +0200

    Remove ROMIMG as it is part of ps2sdk

--- ROMIMG/platform.h
+++
@@ -1,5 +0,0 @@
-int GetUsername(char *buffer, unsigned int BufferSize);
-int GetLocalhostName(char *buffer, unsigned int BufferSize);
-unsigned int GetSystemDate(void);
-unsigned int GetFileCreationDate(const char *path);
-int GetCurrentWorkingDirectory(char *buffer, unsigned int BufferSize);
